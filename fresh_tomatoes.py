7bdf6b064e1b2b52159ad2d98ec5c76b57a42eac
Hitesh Patel <hiteshpat4@gmail.com>
2015-06-06 14:17:25 -0700

    chnaged the navbar title

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -105,7 +105,8 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+              <!--changed title-->
+            <a class="navbar-brand" href="#">Hitesh Patel Moive List</a>
           </div>
         </div>
       </div>
