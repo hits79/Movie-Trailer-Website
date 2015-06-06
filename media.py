0d69cda4cf75d124628d9eb249dab3a6617e9747
Hitesh Patel <hiteshpat4@gmail.com>
2015-06-06 13:12:24 -0700

    Initial commit

---
+++ media.py
@@ -0,0 +1,20 @@
+#importing webbrowser, so we can open the broswer and show trailers
+import webbrowser
+
+class Movie():
+	
+	#def init and adding arguements to class
+	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+
+		#movie title
+		self.title = movie_title
+		#summary of the movie's story
+		self.storyline = movie_storyline
+		#image url of the movie poster
+		self.poster_image_url = poster_image
+		#movie trailer on youtube
+		self.trailer_youtube_url = trailer_youtube
+		
+        #method to show trailers of videos
+	def show_trailer(self):
+                webbrowser.open(self.trailer_youtube_url)
